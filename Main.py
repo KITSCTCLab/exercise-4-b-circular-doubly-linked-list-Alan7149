e9af384f1d266f26282c84e537bc59fbb1c618a9
Alan Babu <97074137+Alan7149@users.noreply.github.com>
2022-08-12 21:13:56 +0530

    5th Commit

--- Main.py
+++ Main.py
@@ -7,7 +7,7 @@ actual_list = []
 
 value = 0
 while len(actual_list) < length_of_circular_linked_list and value < len(circular_linked_list):
-    element = circular_linked_list[index]
+    element = circular_linked_list[value]
     if element not in actual_list:
         actual_list.append(element)
     value += 1
